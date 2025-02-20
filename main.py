7b8764d897dd450b370c126558487c370572b824
Francescaparty <Francescaparticelli6@gmail.com>
2025-02-20 10:10:19 +0100

    Update main.py
    I made some changes in the test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hi there this is a clone test")
+
+
+print("I used the terminal to clone this repository")
+
+print("\nLet's calculate the numbers")
+
+x = 6
+
+y = 7
+
+print(x+y)
+
+print(y-x)
